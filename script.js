56731ec49d253ea04054fc0efa7ba927239a6e94
Kraken57 <kasaad1609@gmail.com>
2024-08-17 02:18:12 +0530

    feat: made Freeform:Scribble
    TODO:a bug of click to fix

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const canvasProps = {
   center: {
     x: window.innerWidth / 2,
     y: window.innerHeight / 2,
-  },    
+  },
 };
 
 const stageProps = {
-    width: 600,
-    height: 480,
-    left: canvasProps.center.x - 600/2,
-    top: canvasProps.center.y - 480/2,
-  };
+  width: 600,
+  height: 480,
+  left: canvasProps.center.x - 600 / 2,
+  top: canvasProps.center.y - 480 / 2,
+};
 
 myCanvas.width = canvasProps.width;
 myCanvas.height = canvasProps.height;
@@ -21,6 +21,37 @@ const ctx = myCanvas.getContext("2d");
 ctx.fillStyle = "gray";
 ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
 
-
 ctx.fillStyle = "white";
-ctx.fillRect(stageProps.left, stageProps.top, stageProps.width, stageProps.height);
+ctx.fillRect(
+  stageProps.left,
+  stageProps.top,
+  stageProps.width,
+  stageProps.height
+);
+
+const route = [];
+
+myCanvas.addEventListener("pointerdown", function (e) {
+  const mousePos = {
+    x: e.offsetX,
+    y: e.offsetY,
+  };
+  route.push(mousePos);
+});
+
+myCanvas.addEventListener("pointermove", function (e) {
+  const mousePos = {
+    x: e.offsetX,
+    y: e.offsetY,
+  };
+  route.push(mousePos);
+});
+
+myCanvas.addEventListener("pointerup", function (e) {
+  ctx.beginPath();
+  ctx.moveTo(route[0].x, route[0].y);
+  route.forEach((point) => {
+    ctx.lineTo(point.x, point.y);
+  });
+  ctx.stroke();
+});
