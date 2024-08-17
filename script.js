8dd617f67c5ef779f8a0cae3f8426e8b69df7d91
Kraken57 <kasaad1609@gmail.com>
2024-08-17 14:22:45 +0530

    feat: Implemented draw Rectangle feature

--- script.js
+++ script.js
@@ -22,32 +22,35 @@ clearCanvas();
 
 const shapes = [];
 let route = [];
+let rectangle = {};
 
+// to draw a rectangle
 myCanvas.addEventListener("pointerdown", function (e) {
   const mousePos = {
     x: e.offsetX,
     y: e.offsetY,
   };
-  route.push(mousePos);
+  rectangle.corner1 = mousePos;
 
   const downCallBack = function (e) {
     const mousePos = {
       x: e.offsetX,
       y: e.offsetY,
     };
-    route.push(mousePos);
+    rectangle.corner2 = mousePos;
 
     clearCanvas();
 
-    for (const shape of [...shapes, route]) {
+    for (const shape of [...shapes, rectangle]) {
       ctx.beginPath();
       ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
       ctx.lineWidth = 5;
-      ctx.moveTo(shape[0].x, shape[0].y);
-      for (let i = 1; i < shape.length; i++) {
-        ctx.lineTo(shape[i].x, shape[i].y);
-      }
-
+      const rect = shape;
+      const minX = Math.min(rect.corner1.x, rect.corner2.x);
+      const minY = Math.min(rect.corner1.y, rect.corner2.y);
+      const width = Math.abs(rect.corner1.x - rect.corner2.x);
+      const height = Math.abs(rect.corner1.y - rect.corner2.y);
+      ctx.rect(minX, minY, width, height);
       ctx.stroke();
     }
   };
@@ -56,13 +59,55 @@ myCanvas.addEventListener("pointerdown", function (e) {
     myCanvas.removeEventListener("pointermove", downCallBack);
     myCanvas.removeEventListener("pointerup", upCallBack);
 
-    shapes.push(route);
-    route = [];
+    shapes.push(rectangle);
+    rectangle = {};
   };
 
   myCanvas.addEventListener("pointermove", downCallBack);
   myCanvas.addEventListener("pointerup", upCallBack);
 });
+
+// to draw a line(Freeform)
+// myCanvas.addEventListener("pointerdown", function (e) {
+//   const mousePos = {
+//     x: e.offsetX,
+//     y: e.offsetY,
+//   };
+//   route.push(mousePos);
+
+//   const downCallBack = function (e) {
+//     const mousePos = {
+//       x: e.offsetX,
+//       y: e.offsetY,
+//     };
+//     route.push(mousePos);
+
+//     clearCanvas();
+
+//     for (const shape of [...shapes, route]) {
+//       ctx.beginPath();
+//       ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+//       ctx.lineWidth = 5;
+//       ctx.moveTo(shape[0].x, shape[0].y);
+//       for (let i = 1; i < shape.length; i++) {
+//         ctx.lineTo(shape[i].x, shape[i].y);
+//       }
+
+//       ctx.stroke();
+//     }
+//   };
+
+//   const upCallBack = function (e) {
+//     myCanvas.removeEventListener("pointermove", downCallBack);
+//     myCanvas.removeEventListener("pointerup", upCallBack);
+
+//     shapes.push(route);
+//     route = [];
+//   };
+
+//   myCanvas.addEventListener("pointermove", downCallBack);
+//   myCanvas.addEventListener("pointerup", upCallBack);
+// });
 
 function clearCanvas() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
